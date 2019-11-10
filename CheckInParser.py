651617dd2134e1614edc13a9fd9ffcf288c8db26
dmitrifurman <dmitri_furman@hotmail.com>
2019-11-09 21:02:51 -0600

    version that parses check in and creates a table

--- CheckInParser.py
+++ CheckInParser.py
@@ -1,24 +1,45 @@
 import re
+from collections import defaultdict
 
-checkins = dict()
-read_data = []
-sidre = re.compile(r'[a-zA-Z]\d{6,6}')
-checkinre = re.compile(r'[a-zA-Z]\d{6,6}\s*#justcode\d+')
+checkins = defaultdict(list)
+#sidre = re.compile(r'[a-zA-Z]\d{6,6}')
+checkinre = re.compile(r'[a-zA-Z]\d{6,6}\s*#justcode@week\d+')
 
-def parseSid(line):
-    sidp = sidre.search(line);
-    print(sidp)
-    if sidp is not None:
-        return sidp.group()
+def parseSid(checkin):
+    return checkin[0:7]
 
-
-
-def parseLine(line):
-    print(line)
-    sid = parseSid(line)
-    print(sid)
+def parseWeek(checkin):
+    # this has to be updated for challanges more than 10 weeks
+    return checkin[-1]
 
 
-with open('checkins.txt') as f:
-    for line in f:
-        parseLine(line)
+def parseChallenge(line):
+    ''' This functional parses the name of the challange if multiple challenges are running'''
+
+def addCheckin(checkin):
+    sid = parseSid(checkin)
+    week = parseWeek(checkin)
+    #This is future features when supporting multiple challanges
+    #parseChallenge(checkin)
+    checkins[sid].append(week)
+
+def parseCheckin(line):
+    checkinp = checkinre.search(line)
+    if checkinp is not None:
+        checkin = checkinp.group()
+        addCheckin(checkin)
+
+def processCheckins():
+    with open('checkins.txt') as f:
+        for line in f:
+            parseCheckin(line)
+    # there is likely a better python like way thank this
+    for key, value in sorted(checkins.items()):
+        print(key, sorted(value))
+
+def main():
+    processCheckins()
+
+# main function calling
+if __name__ == "__main__":
+    main()
